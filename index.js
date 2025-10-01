1a55d752a95a009a2e68628b268c795eec1a27a7
“josy216” <josyabteka@gmail.com>
2025-10-01 06:11:12 +0300

    updated

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ dotenv.config();
 import router from './route/router.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin:"https://jocode-hoteladmin.netlify.app",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+    
+}));
 app.use(express.json());
 app.use('/api', router);
 
